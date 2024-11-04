9400a67befca95065e8526033617a5427379cf4c
Mayya <maya1999@mail.ru>
2024-11-04 15:27:23 +0300

    Перенос проекта на React

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className='right'>rigth side</div>
+      <div className='left'>left</div>
+    </div>
+  );
+}
+
+export default App;
